74c20e20d90942a56a7a6c931558a0dd6f5a5f28
Balaguru4580 <balaguru4580@gmail.com>
2020-10-01 00:34:16 +0530

    Base Logic Working

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+    var finalURL;
+    var URLReceiver = document.getElementById('Ourl');
+    var SourceReceiver = document.getElementById('src');
+    var EmailReceiver = document.getElementById('email');
 
+    
+    URLReceiver.onkeyup = function(){aggregrator()};
+    SourceReceiver.onkeyup = function(){aggregrator()};
+    EmailReceiver.onkeyup = function(){aggregrator()};
+
+    function aggregrator()
+    {
+        finalURL = URLReceiver.value + SourceReceiver.value + EmailReceiver.value;
+        document.getElementById('outputURL').innerHTML = finalURL;
+        console.log("MANE");
+    }
+
+    
